493a2b0458b09ae03331ebc80aa542bdca09aba6
DonLemos <142523535+DonLemos@users.noreply.github.com>
2024-01-25 10:39:57 +0200

    Add files via upload

---
+++ Intro To Programming/Javascript for Kids/chapter 1/Chapter1.js
@@ -0,0 +1,13 @@
+//CHAPTER 1
+//INTRO
+
+//let myString = "Hello World";
+//console.log(myString);
+
+// //Draw as many cats as you want!
+// var drawCats = function (howManyTimes) {
+//     for (let i = 0; i < howManyTimes; i++) {
+//         console.log(i + " =^.^=");
+//     }
+// };
+// drawCats(15);
